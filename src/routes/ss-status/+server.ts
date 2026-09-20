ccb4ffb58f614519ff27473500cf2f5694130d66
PXR <pxr2709@gmail.com>
2026-09-20 22:19:29 +0700

    cors

--- src/routes/ss-status/+server.ts
+++ src/routes/ss-status/+server.ts
@@ -1,3 +1,20 @@
 export async function GET() {
-	return new Response(JSON.stringify({ active: false }));
+	return new Response(JSON.stringify({ active: false }), {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*'
+		}
+	});
+}
+
+export async function OPTIONS() {
+	return new Response(null, {
+		status: 204,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type, Accept'
+		}
+	});
 }
